5b1988b4687bb8505c6ee0d3ba1b046792a22566
Abonia Sojasingarayar <aboniaa@gmail.com>
2019-02-19 14:24:00 +0100

    canny edge calculation

--- lanes.py
+++ lanes.py
@@ -22,8 +22,8 @@ lane_image=np.copy(image)
 
 gray=cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
 blur=cv2.GaussianBlur(gray,(5,5),0)
-#canny=cv2.Canny(blur,50,150)
-#cv2.imshow("result",canny)
+canny=cv2.Canny(blur,50,150)
+cv2.imshow("result",canny)
 cv2.waitKey(0)
 
 # plt.imshow(canny)
