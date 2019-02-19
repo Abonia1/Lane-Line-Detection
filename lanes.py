4d3c3d502ee89ae689eb9e151544e018dc612d06
Abonia Sojasingarayar <aboniaa@gmail.com>
2019-02-19 14:55:06 +0100

    Fillpoly fill single polygon

--- lanes.py
+++ lanes.py
@@ -10,18 +10,20 @@ def canny(image):
     return canny
 
 def region_of_interest(image):
-    #height=image.shape(1)
-    #polygons=np.array([(200,height),(1100,height),(550,250)])
-    mask=np.zeros.like(image)
-    #cv2.fillPoly(mask,polygons,255)
+    height=image.shape[0]
+    polygons=np.array([[(200,height),(1100,height),(550,250)]])
+    mask=np.zeros_like(image)
+    """to fill polygon with mask"""
+    cv2.fillPoly(mask,polygons,255)
     return mask
 
 image=cv2.imread('img.png')
 lane_image=np.copy(image)
 canny=canny(lane_image)
 
-plt.imshow(canny)
-plt.show()
+
+cv2.imshow("result",region_of_interest(canny))
+cv2.waitKey(0)
 
 # plt.imshow(canny)
 # plt.show(0)
