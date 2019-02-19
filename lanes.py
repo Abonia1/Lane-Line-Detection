76081131c42ca3d5217dfa1374ecba35932db01d
Abonia Sojasingarayar <aboniaa@gmail.com>
2019-02-19 14:42:29 +0100

    matplot to find region of interest

--- lanes.py
+++ lanes.py
@@ -18,13 +18,10 @@ def region_of_interest(image):
 
 image=cv2.imread('img.png')
 lane_image=np.copy(image)
-#canny=canny(lane_image)
+canny=canny(lane_image)
 
-gray=cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
-blur=cv2.GaussianBlur(gray,(5,5),0)
-canny=cv2.Canny(blur,50,150)
-cv2.imshow("result",canny)
-cv2.waitKey(0)
+plt.imshow(canny)
+plt.show()
 
 # plt.imshow(canny)
 # plt.show(0)
